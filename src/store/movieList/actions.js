1e40fa56be334522aad00da58df8aa85db4ab5e5
Vadim <vadim10sakuta@gmail.com>
2021-04-01 17:23:30 +0300

    changed action creators to arrow functions

--- src/store/movieList/actions.js
+++ src/store/movieList/actions.js
@@ -2,23 +2,23 @@ export const CHANGE_PAGE = 'CHANGE_PAGE';
 export const SET_TOTAL_PAGES = 'SET_TOTAL_PAGES';
 export const SET_POPULAR_MOVIES = 'SET_POPULAR_MOVIES';
 
-export function changePage(page) {
+export const changePage = (page) => {
   return {
     type: CHANGE_PAGE,
     payload: page,
   };
-}
+};
 
-export function setTotalPages(totalPages) {
+export const setTotalPages = (totalPages) => {
   return {
     type: SET_TOTAL_PAGES,
     payload: totalPages,
   };
-}
+};
 
-export function setPopularMovies(movies) {
+export const setPopularMovies = (movies) => {
   return {
     type: SET_POPULAR_MOVIES,
     payload: movies,
   };
-}
+};
