d6551afd421557706416f92286656e89a347218c
Vadim <vadim10sakuta@gmail.com>
2021-04-10 13:11:12 +0300

    fixed styles

--- src/components/Page404/Page404.jsx
+++ src/components/Page404/Page404.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './Page404.scss';
 
 const Page404 = () => {
   return (
-    <div className='page404 d-flex flex-column justify-content-center align-items-center'>
+    <div className='vh-100 vw-100 d-flex flex-column justify-content-center align-items-center'>
       <h2 className='h2'>Not found</h2>
       <h2 className='h2'>404</h2>
     </div>
