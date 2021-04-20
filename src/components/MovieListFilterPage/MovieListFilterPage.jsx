e785e8c10503f66aad92042283d36e12a1daee72
Vadim <vadim10sakuta@gmail.com>
2021-04-21 01:05:02 +0300

    fixed parse, stringify search string logic

--- src/components/MovieListFilterPage/MovieListFilterPage.jsx
+++ src/components/MovieListFilterPage/MovieListFilterPage.jsx
@@ -8,7 +8,10 @@ import {
   Spinner,
 } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectGenresData } from '../../store/genres/selectors';
+import {
+  selectGenresData,
+  selectGenresLoading,
+} from '../../store/genres/selectors';
 import { loadGenres } from '../../store/genres/effects';
 import { useHistory, useLocation } from 'react-router-dom';
 import {
@@ -26,17 +29,27 @@ import {
 } from '../../store/movieList/selectors';
 import MovieList from '../MovieList/MovieList';
 import { changePage, updateData } from '../../store/movieList/actions';
-import { createParamObj, getSelectedValues } from '../../utils/selectUtils';
+import {
+  createParamObj,
+  createValuesSelectField,
+  getDefaultValuesSelectField,
+} from '../../utils/selectUtils';
+import clsx from 'clsx';
 
 const MovieListFilterPage = () => {
   const { search, pathname } = useLocation();
   const history = useHistory();
   const genres = useSelector(selectGenresData);
+  const isGenresLoading = useSelector(selectGenresLoading);
   const movies = useSelector(selectMovieList);
   const isMoviesLoading = useSelector(selectMovieListLoading);
   const currentPage = useSelector(selectCurrentPage);
   const totalPages = useSelector(selectTotalPages);
   const dispatch = useDispatch();
+  const containerClasses = clsx(
+    'pt-2 pb-2',
+    isGenresLoading && 'd-flex justify-content-center'
+  );
 
   useEffect(() => {
     dispatch(loadGenres());
@@ -60,8 +73,8 @@ const MovieListFilterPage = () => {
     const paramStr = stringifyGetParamsObj(paramObj);
     if (paramStr !== search) {
       dispatch(updateData());
+      paramObj.page = 1;
     }
-    paramObj.page = 1;
     history.replace({ pathname, search: stringifyGetParamsObj(paramObj) });
   };
 
@@ -76,93 +89,99 @@ const MovieListFilterPage = () => {
     id: el,
     name: el,
   }));
-
-  const fieldsStructure = {
-    with_genres: {
-      data: genres,
-      isShouldArray: true,
-    },
-    'vote_average.gte': {
-      data: votesAverageArr,
-      isShouldArray: true,
-    },
-    'vote_average.lte': {
-      data: votesAverageArr,
-      isShouldArray: true,
-    },
-  };
-
-  const selectedValues = getSelectedValues(fieldsStructure);
-  const genresOptions = genres.map((g) => ({ value: g.id, label: g.name }));
-  const voteAverageOptions = votesAverageArr.map((v) => ({
-    value: v.id,
-    label: v.name,
-  }));
+  const defaultValues = parseGetParamsStr(search);
+  const defaultGenres = getDefaultValuesSelectField(
+    defaultValues.with_genres,
+    genres
+  );
+  const defaultVoteAverageGte = getDefaultValuesSelectField(
+    defaultValues['vote_average.gte'],
+    votesAverageArr
+  );
+  const defaultVoteAverageLte = getDefaultValuesSelectField(
+    defaultValues['vote_average.lte'],
+    votesAverageArr
+  );
 
   return (
-    <Container className='pt-2 pb-2'>
-      <Row>
-        <Col>
-          <Accordion>
-            <Row className='mb-1'>
-              <Col>
-                <Accordion.Toggle
-                  as={Button}
-                  variant='light'
-                  className='d-flex align-items-center'
-                  eventKey='0'
-                >
-                  <span className='mr-1'>Filters</span>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    height='24px'
-                    viewBox='0 0 24 24'
-                    width='24px'
-                    fill='#000000'
-                  >
-                    <path d='M0 0h24v24H0V0z' fill='none' />
-                    <path d='M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z' />
-                  </svg>
-                </Accordion.Toggle>
-              </Col>
-            </Row>
-            <Row>
-              <Col className=''>
-                <Accordion.Collapse eventKey='0'>
-                  <FilterForm
-                    onSubmit={onSubmit}
-                    defaultValues={selectedValues}
-                    values={{
-                      with_genres: genresOptions,
-                      'vote_average-gte': voteAverageOptions,
-                      'vote_average-lte': voteAverageOptions,
-                    }}
-                    isLoading={isMoviesLoading}
-                  />
-                </Accordion.Collapse>
-              </Col>
-            </Row>
-          </Accordion>
-        </Col>
-      </Row>
-      <Row>
-        <Col className='p-0 pt-1 d-flex justify-content-center'>
-          {isMoviesLoading ? (
-            <Spinner animation='border' variant='success' />
-          ) : movies.length ? (
-            <MovieList
-              currentPage={currentPage}
-              totalPages={totalPages}
-              movies={movies}
-              onChangePage={onChangePage}
-            />
-          ) : (
-            <span className='text-center font-weight-bold text-secondary'>
-              Nothing found
-            </span>
-          )}
-        </Col>
-      </Row>
+    <Container className={containerClasses}>
+      {isGenresLoading ? (
+        <Spinner animation='border' variant='success' />
+      ) : (
+        <>
+          <Row>
+            <Col>
+              <Accordion>
+                <Row className='mb-1'>
+                  <Col>
+                    <Accordion.Toggle
+                      as={Button}
+                      variant='light'
+                      className='d-flex align-items-center'
+                      eventKey='0'
+                    >
+                      <span className='mr-1'>Filters</span>
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        height='24px'
+                        viewBox='0 0 24 24'
+                        width='24px'
+                        fill='#000000'
+                      >
+                        <path d='M0 0h24v24H0V0z' fill='none' />
+                        <path d='M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z' />
+                      </svg>
+                    </Accordion.Toggle>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Accordion.Collapse eventKey='0'>
+                      <FilterForm
+                        onSubmit={onSubmit}
+                        defaultValues={{
+                          with_genres: defaultGenres,
+                          'vote_average-gte': defaultVoteAverageGte,
+                          'vote_average-lte': defaultVoteAverageLte,
+                          page: defaultValues.page,
+                        }}
+                        values={{
+                          with_genres: createValuesSelectField(genres),
+                          'vote_average-gte': createValuesSelectField(
+                            votesAverageArr
+                          ),
+                          'vote_average-lte': createValuesSelectField(
+                            votesAverageArr
+                          ),
+                        }}
+                        isLoading={isMoviesLoading}
+                      />
+                    </Accordion.Collapse>
+                  </Col>
+                </Row>
+              </Accordion>
+            </Col>
+          </Row>
+          <Row>
+            <Col className='p-0 pt-1 d-flex justify-content-center'>
+              {isMoviesLoading ? (
+                <Spinner animation='border' variant='success' />
+              ) : movies.length ? (
+                <MovieList
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  movies={movies}
+                  onChangePage={onChangePage}
+                />
+              ) : (
+                <span className='text-center font-weight-bold text-secondary'>
+                  Nothing found
+                </span>
+              )}
+            </Col>
+          </Row>
+        </>
+      )}
     </Container>
   );
 };
--- src/utils/selectUtils.js
+++ src/utils/selectUtils.js
@@ -1,58 +1,45 @@
-import { parseGetParamsStr } from './utils';
-
-export const getParamObj = () => {
-  const paramObj = parseGetParamsStr(window.location.search);
-  Object.entries(paramObj).forEach(([key, value]) => {
-    if (value.includes('|')) {
-      paramObj[key] = value.split('|');
-      return;
-    }
-    paramObj[key] = value;
-  });
-  return paramObj;
-};
-
 export const createParamObj = (data) => {
   return Object.entries(data).reduce((acc, [key, value]) => {
     key = key.replace('-', '.');
 
-    if (Array.isArray(value)) {
+    if (Array.isArray(value) && value.length) {
       return { ...acc, [key]: value.map((v) => v.value).join('|') };
-    } else if (typeof value === 'object') {
+    } else if (typeof value === 'object' && value.hasOwnProperty('value')) {
       return { ...acc, [key]: value.value };
     }
     return { ...acc, [key]: value };
   }, {});
 };
 
-export function getSelectedValues(valuesObj) {
-  const paramObj = getParamObj();
+/**
+ * @param initialValues should have such structure as [{id:, name:}...]
+ */
+export function createValuesSelectField(initialValues) {
+  return initialValues.map((v) => ({
+    value: v.id,
+    label: v.name,
+  }));
+}
 
-  return Object.entries(paramObj).reduce((acc, [key, value]) => {
-    const getSelectedObj = (id) => {
-      id = id !== '' ? +id : id;
-      const index = valuesObj[key].data.findIndex((v) => id === v.id);
-      if (index !== -1) {
-        return {
-          value: valuesObj[key].data[index].id,
-          label: valuesObj[key].data[index].name,
-        };
+/**
+ * @param defaultStr contains ids string of values, ids could to separate by symbol |
+ * @param values should have such structure as [{id:, name:}...]
+ */
+export function getDefaultValuesSelectField(defaultStr = '', values) {
+  const ids = defaultStr.split('|');
+  return ids.reduce((acc, id) => {
+    if (id) {
+      const defaultValueIndex = values.findIndex((v) => v.id === +id);
+      if (defaultValueIndex !== -1) {
+        return [
+          ...acc,
+          {
+            value: values[defaultValueIndex].id,
+            label: values[defaultValueIndex].name,
+          },
+        ];
       }
-      return null;
-    };
-
-    if (Array.isArray(value)) {
-      return {
-        ...acc,
-        [key.replace('.', '-')]: value.reduce((acc, vParam) => {
-          let obj = getSelectedObj(vParam);
-          return obj ? [...acc, obj] : acc;
-        }, []),
-      };
-    } else if (valuesObj[key] && valuesObj[key].isShouldArray) {
-      let obj = getSelectedObj(value);
-      return obj ? { ...acc, [key.replace('.', '-')]: [obj] } : acc;
     }
-    return { ...acc, [key.replace('.', '-')]: value };
-  }, {});
+    return acc;
+  }, []);
 }
