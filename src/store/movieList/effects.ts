e93008d67d5387317f4c085f835a90cb6408a9a2
Vadim <vadim10sakuta@gmail.com>
2021-05-31 23:46:57 +0300

    fixes

--- src/store/movieList/effects.ts
+++ src/store/movieList/effects.ts
@@ -17,7 +17,7 @@ import { setAppError } from '../app/actionCreators';
 export const loadMoviesByType = (movieType: string, paramsObj: ParamGetObj) => {
   return async (dispatch: Dispatch<any>) => {
     try {
-      dispatch(loadMovies(ApiMovies.GET[movieType.toUpperCase()]));
+      dispatch(loadMovies(ApiMovies.GET[movieType.toUpperCase()], paramsObj));
     } catch (e) {
       console.log(e);
       dispatch(setAppError(true));
@@ -28,7 +28,7 @@ export const loadMoviesByType = (movieType: string, paramsObj: ParamGetObj) => {
 export const loadDiscoverMovies = (paramsObj: ParamGetObj) => {
   return async (dispatch: Dispatch<any>) => {
     try {
-      dispatch(loadMovies(ApiMovies.GET.DISCOVER, parseGetParamsStr(paramStr)));
+      dispatch(loadMovies(ApiMovies.GET.DISCOVER, paramsObj));
     } catch (e) {
       console.log(e);
       dispatch(setAppError(true));
@@ -45,7 +45,6 @@ export const loadUserBasicMovieList = (
       const account_id = selectUserDataDetails(getState()).id;
       paramsObj.session_id = String(ApiAuth.getSessionId());
       dispatch(loadMovies(ApiAccount.GET[type](account_id), paramsObj));
-      dispatch(loadMovies(ApiAccount.GET[type](account_id), paramsObj));
     } catch (e) {
       console.log(e);
       dispatch(setAppError(true));
@@ -54,13 +53,9 @@ export const loadUserBasicMovieList = (
 };
 
 export const loadMovies = (URL: string, paramsObj = {}) => {
-  return async (
-    dispatch: Dispatch<MovieListAction | SetAppErrorAction>,
-    getState: GetRootState
-  ) => {
+  return async (dispatch: Dispatch<MovieListAction | SetAppErrorAction>) => {
     try {
       dispatch(setMoviesTypeLoading(true));
-      console.log(paramsObj);
 
       const data = await ApiMovies.loadMovieList(URL, paramsObj);
 
